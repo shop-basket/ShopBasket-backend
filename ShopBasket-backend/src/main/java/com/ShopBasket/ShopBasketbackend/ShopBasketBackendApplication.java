05d29b61afca0b2ffe01e08d4c96a4624eef776f
pavit <pavitharan2020@gmail.com>
2023-11-04 20:24:20 +0530

    my first commit

---
+++ ShopBasket-backend/src/main/java/com/ShopBasket/ShopBasketbackend/ShopBasketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ShopBasket.ShopBasketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBasketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBasketBackendApplication.class, args);
+	}
+
+}
---
+++ ShopBasket-backend/src/test/java/com/ShopBasket/ShopBasketbackend/ShopBasketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShopBasket.ShopBasketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBasketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
